c8d5fecfbc3580a4fa579a7e0eddef7e02adc5ad
Timur Zakirov <timurzakirov@Timurs-MacBook-Air.local>
2020-07-13 17:39:13 +0300

    beta

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 8080;
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.listen(PORT, () => {
+  console.log(`server on port ${PORT}`);
+});
